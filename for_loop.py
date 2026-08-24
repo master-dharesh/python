c4194d6b7bf45ddd33e69554d327bbc3e4c2cb6d
dharesh <dhareshbalaraman@gmail.com>
2026-08-24 19:17:15 +0530

    file update1

--- for_loop.py
+++ for_loop.py
@@ -1,6 +1,16 @@
-fruit = ["apple", "banana", "orange"]
-#fruit ="apple"
-for i in fruit:
- print(i)
- if i == "banana":
+#fruit = ["apple", "banana", "orange"]
+##fruit ="apple"
+#for i in fruit:
+# print(i)
+# if i == "banana":
+#  break
+
+#for i in range(1, 11):
+# print("hello")
+
+for table in range(1, 11):
+ print(table*2)
+ if table ==5:
   break
+
+
---
+++ while_loop.py
@@ -0,0 +1,8 @@
+num = int(input("enter your number"))
+i = 1
+while i <= num:
+ print(i*5)
+ if i==10:
+  break
+ i=i+1
+
