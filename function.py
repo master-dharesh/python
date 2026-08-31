9ddcbb57a41f18e25be7aea02df996b05f4eb8f3
dharesh <dhareshbalaraman@gmail.com>
2026-08-31 19:38:35 +0530

    file update2

---
+++ function.py
@@ -0,0 +1,18 @@
+def add(x, y):
+  return x+y
+
+#result = add(12, 11)
+#print(result)
+#print(add(11, 13))
+#def wellcom():
+# print("hello this is python class")
+
+	#wellcom()
+
+def even():
+ for i in range(1, 21):
+  if i%2==0:
+   print(i)
+
+result = even()
+print(result)
--- while_loop.py
+++ while_loop.py
@@ -1,8 +1,14 @@
-num = int(input("enter your number"))
-i = 1
-while i <= num:
- print(i*5)
- if i==10:
-  break
- i=i+1
+#num = int(input("enter your number"))
+#i = 1
+#while i <= num:
+# print(i*5)
+# if i==10:
+#  break
+# i=i+1
 
+name = "dharesh"
+while True:
+ user_name = input("enter your name")
+ if user_name == name:
+   print("your ", name)
+   break
