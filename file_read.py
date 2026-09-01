14b2d8cc105042b80614f408d5520e9cdd140153
dharesh <dhareshbalaraman@gmail.com>
2026-09-01 19:31:23 +0530

    file update 3

---
+++ file_read.py
@@ -0,0 +1,4 @@
+file = open("test.txt", "r")
+#file.write("hello i am dharesh")
+
+print(file.read())
--- new_math.py
+++ new_math.py
@@ -1,4 +1,5 @@
 import math
 square_root = math.sqrt(4)
-print(square_root)
-print(math.pow(5, 2))
+#print(square_root)
+#print(math.pow(5, 2))
+print(dir(math))
